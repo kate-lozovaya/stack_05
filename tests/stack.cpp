b201a41f80516ac2e3a96ea932ddf6a28727cdcd
kate-lozovaya <katekalozovaya@gmail.com>
2017-11-03 20:10:38 +0300

    Update stack.cpp

--- tests/stack.cpp
+++ tests/stack.cpp
@@ -2,8 +2,20 @@
 
 #include "stack.hpp"
 
-TEST_CASE("empty method for empty stack", "")
+TEST_CASE("push") 
 {
-    stack<int> s;
-    REQUIRE( s.empty() );
+    stack<int> Stack;
+    Stack.push(5);
+    Stack.push(7);
+    REQUIRE(Stack.count() == 2);
+}
+
+TEST_CASE("pop") 
+{
+    stack<int> Stack;
+    Stack.push(5);
+    Stack.push(7);
+    Stack.push(9);
+    Stack.pop();
+    REQUIRE(Stack.count() == 2);
 }
